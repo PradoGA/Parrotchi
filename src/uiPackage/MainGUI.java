144f4a6d1163890f2163c6610251998f08e64c16
Gabriela Prado <150290516+PradoGabriela@users.noreply.github.com>
2025-02-05 15:02:19 +0000

    starting UI

---
+++ src/uiPackage/MainGUI.java
@@ -0,0 +1,19 @@
+package uiPackage;
+
+import javax.swing.*;
+
+public class MainGUI {
+    private JPanel panel1;
+    private JPanel basePanel;
+    private JPanel topBar;
+    private JSlider slider1;
+    private JButton button1;
+    private JPanel cardPanel;
+    private JPanel bottomBar;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JLabel feedNotifier;
+    private JLabel playNotifier;
+}
